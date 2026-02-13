b9a3b5245d2cd0fb3a681ee1c639cb39feb812cb
udnonme-byte <udnonme@gmail.com>
2026-02-14 01:19:07 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,20 +2,20 @@ import { assets } from "$app/paths";
 
 export const config = {
 	mainPage: {
-		heading: "Hello!",
-		invitation: "Will you go on a date with me this week? 👉👈🥺",
-		yesText: "Yes",
-		noText: "No",
+		heading: "Дианчик мой любимкинс!",
+		invitation: "будешь ли ты моей на веки вечные?👉👈🥺",
+		yesText: "Да",
+		noText: "Нет",
 		topImage: `${assets}/Assets/Top_picture/Cute_pic2.jpg`,
 		leftHeart: `${assets}/Assets/Hearts/heart_3d.gif`,
 		rightHeart: `${assets}/Assets/Hearts/heart_3d.gif`,
 	},
 	noPage: {
-		message: '"No" is not really an option😁',
+		message: '"нет" ваще не варик😁',
 		image: `${assets}/Assets/No_picture/kermit-shooting.gif`,
 	},
 	yesPage: {
-		message: "See you this week 😄",
+		message: "умница, любимкинс❤❤❤😄",
 		image: `${assets}/Assets/Yes_picture/Yes.gif`,
 	},
 };
